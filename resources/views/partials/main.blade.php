b18656664289d0cb9d18f5821912b46058cc48a5
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-24 18:53:32 +0200

    Migliorato stile del main, sez. comics

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -3,18 +3,20 @@
     <img class="jumbotron" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
     <!-- Fine jumbotron -->
     <!-- Inizio ciclo foreach per fumetti -->
-    <div class="container">
-        <div class="row">           
-            @foreach($comics as $comic)
-                <div class="col-12 col-md-6 col-lg-2 mx-2">
-                    <div class="card my-2">
-                        <img class="img-fluid card-img-top" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                        <div class="card-body">
-                            <h6 class="card-title">{{ $comic['title'] }}</h6>
+    <div class="bg-black">
+        <div class="container">
+            <div class="row">           
+                @foreach($comics as $comic)
+                    <div class="col-12 col-md-6 col-lg-2 mt-5">
+                        <div class="my-1">
+                            <img class="img-fluid card-img-top h_img" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                            <div>
+                                <span class="card-title text-white">{{ $comic['title'] }}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
     <!-- fine sezione fumetti -->

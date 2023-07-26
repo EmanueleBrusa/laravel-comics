569ee4045c9d4c8a6e68bb06ad927e340f3bf85c
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-26 16:39:02 +0200

    Creazione route e prova collegamento show.blade

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    Bla bla prova
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,15 @@
                 <!-- inizio badge -->
                 <div class="badge col-2 text-center"> CURRENT SERIES</div>
                 <!-- fine badge -->       
-                @foreach($comics as $comic)
+                @foreach($comics as $id => $comic)
                     <div class="col-12 col-md-6 col-lg-2 mt-5">
                         <div class="my-1">
-                            <img class="img-fluid card-img-top h_img" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                            <div>
-                                <span class="card-title text-white">{{ $comic['title'] }}</span>
-                            </div>
+                            <a href="{{ route('comics.show', $id)}}">
+                                <img class="img-fluid card-img-top h_img" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                                <div>
+                                    <span class="card-title text-white">{{ $comic['title'] }}</span>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 @endforeach

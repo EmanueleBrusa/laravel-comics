768d49759f6cb38476d21ed9b7b160f47aaec910
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-26 17:09:04 +0200

    Fine esercizio (provvisorio)

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    Bla bla prova
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-8">
+                        <img class="w-50" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        <div class="p-3 mt-4">
+                        <h4 class="text-primary"><strong>{{ $comic['title'] }}</strong></h4>
+                            <p class="text-success"><strong>Prezzo:  </strong>{{ $comic['price'] }}</p>
+                            <p><strong>Serie:  </strong>{{ $comic['series'] }}</p>
+                            <p><strong>Tipo:  </strong>{{ $comic['type'] }}</p>
+                            <p><strong>Data uscita:  </strong>{{ $comic['sale_date'] }}</p>
+                            <p><strong>Descrizione:  </strong>{{ $comic['description'] }}</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <h6 class="text-secondary"><strong>ADVERTISMENT</strong></h6>
+                        <img class="w-100" src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
